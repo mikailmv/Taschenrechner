3c4a4cfb483f4532727b46b02e0c75958efea1a1
mikailmv <mikailmikiev2@gmail.com>
2024-07-27 19:18:03 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Taschenrechner
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.ergebnisTextBox = new System.Windows.Forms.TextBox();
+            this.zahl1TextBox = new System.Windows.Forms.TextBox();
+            this.zahl2TextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // 
+            // ergebnisTextBox
+            // 
+            this.ergebnisTextBox.Location = new System.Drawing.Point(12, 12);
+            this.ergebnisTextBox.Name = "ergebnisTextBox";
+            this.ergebnisTextBox.ReadOnly = true;
+            this.ergebnisTextBox.Size = new System.Drawing.Size(260, 23);
+            this.ergebnisTextBox.TabIndex = 0;
+            this.ergebnisTextBox.Text = "Ergebnis";
+
+            // 
+            // zahl1TextBox
+            // 
+            this.zahl1TextBox.Location = new System.Drawing.Point(12, 41);
+            this.zahl1TextBox.Name = "zahl1TextBox";
+            this.zahl1TextBox.Size = new System.Drawing.Size(120, 23);
+            this.zahl1TextBox.TabIndex = 1;
+
+            // 
+            // zahl2TextBox
+            // 
+            this.zahl2TextBox.Location = new System.Drawing.Point(152, 41);
+            this.zahl2TextBox.Name = "zahl2TextBox";
+            this.zahl2TextBox.Size = new System.Drawing.Size(120, 23);
+            this.zahl2TextBox.TabIndex = 2;
+
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 70);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(60, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+
+            // 
+            // subtractButton
+            // 
+            this.subtractButton.Location = new System.Drawing.Point(78, 70);
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.Size = new System.Drawing.Size(60, 23);
+            this.subtractButton.TabIndex = 4;
+            this.subtractButton.Text = "-";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            this.subtractButton.Click += new System.EventHandler(this.subtractButton_Click);
+
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(144, 70);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(60, 23);
+            this.multiplyButton.TabIndex = 5;
+            this.multiplyButton.Text = "*";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyButton_Click);
+
+            // 
+            // divideButton
+            // 
+            this.divideButton.Location = new System.Drawing.Point(210, 70);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(60, 23);
+            this.divideButton.TabIndex = 6;
+            this.divideButton.Text = "/";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.divideButton_Click);
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 101);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.subtractButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.zahl2TextBox);
+            this.Controls.Add(this.zahl1TextBox);
+            this.Controls.Add(this.ergebnisTextBox);
+            this.Name = "Form1";
+            this.Text = "Taschenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox ergebnisTextBox;
+        private System.Windows.Forms.TextBox zahl1TextBox;
+        private System.Windows.Forms.TextBox zahl2TextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subtractButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button divideButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            FühreOperationDurch(Operation.Addieren);
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            FühreOperationDurch(Operation.Subtrahieren);
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            FühreOperationDurch(Operation.Multiplikation);
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            FühreOperationDurch(Operation.Division);
+        }
+
+        private void FühreOperationDurch(Operation operation)
+        {
+            if (double.TryParse(zahl1TextBox.Text, out double zahl1) &&
+                double.TryParse(zahl2TextBox.Text, out double zahl2))
+            {
+                double ergebnis = 0;
+                switch (operation)
+                {
+                    case Operation.Addieren:
+                        ergebnis = zahl1 + zahl2;
+                        break;
+                    case Operation.Subtrahieren:
+                        ergebnis = zahl1 - zahl2;
+                        break;
+                    case Operation.Multiplikation:
+                        ergebnis = zahl1 * zahl2;
+                        break;
+                    case Operation.Division:
+                        if (zahl2 != 0)
+                        {
+                            ergebnis = zahl1 / zahl2;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Fehler bei der Division: Der Divisor darf nicht null sein.");
+                            return;
+                        }
+                        break;
+                }
+                ergebnisTextBox.Text = ergebnis.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Bitte geben Sie gültige Zahlen ein.");
+            }
+        }
+
+        private enum Operation
+        {
+            Addieren,
+            Subtrahieren,
+            Multiplikation,
+            Division
+        }
+    }
+}
